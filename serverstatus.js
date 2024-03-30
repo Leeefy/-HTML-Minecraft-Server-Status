be874e41354310a96a5d2a480dec3a9ce6f6b98f
Leeefy <165488227+Leeefy@users.noreply.github.com>
2024-03-30 14:32:12 +0000

    Add files via upload

---
+++ serverstatus.js
@@ -0,0 +1,48 @@
+// Fetch API
+async function getServerStatus() {
+	try {
+		const response = await fetch(
+			'https://api.mcsrvstat.us/3/<IP ADDRESS>', // Replace <IP ADDRESS> with the IP Address of your server!
+			{
+				method: 'GET',
+			},
+		);
+// Catch Error
+		if (!response.ok) {
+			throw new Error(`Error! status: ${response.status}`);
+		}
+
+		const data = await response.json();
+
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+}
+// Display
+getServerStatus().then(data => {
+	console.log(data);
+
+	const a = document.createElement('a');
+
+	const b = document.createElement('b');
+
+	const online = document.createElement('a');
+	if (data.online != true) {
+		online.innerHTML = `<style> strong {color:red;} </style> <strong>Offline</strong>`;
+		b.append(...[online]);
+	}
+	else {
+		online.innerHTML = `<style> strong {color:#66cc00;}</style> <strong>Online</strong>`;
+
+		const players = document.createElement('a');
+		players.innerHTML = `<bdi>${data.players.online}/${data.players.max}</bdi>`;
+
+		b.append(...[online, players]);
+	}
+	
+	a.appendChild(b);
+
+	const container = document.getElementById('container');
+	container.appendChild(b);
+});
